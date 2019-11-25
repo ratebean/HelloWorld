c3a4d85454562ebd28817682de77b1aba906a1c7
Douglas Bean <douglas.bean@rate.com>
2019-11-25 15:16:01 -0600

    initial commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HelloWorld put me in master branch");
+
+            Console.WriteLine("press any key..");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HelloWorld/Simple.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Simple
+    {
+        //a more realistic example. I don't expect anton and I to be working on the same file.
+    }
+}
